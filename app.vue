1c28c06a0dcf87b032138d47ab35b6c2f78a2750
Félicien <felicien@alvarum.com>
2023-02-15 21:33:50 +0100

    improve scaling

--- app.vue
+++ app.vue
@@ -1,286 +1,283 @@
 <template>
   <Link rel="shortcut icon" type="image/png" href="/icon.png" />
-  <v-app :style="{ height, background: '#fefefe' }">
-    <v-dialog v-model="dialog" fullscreen transition="dialog-top-transition">
-      <template #activator="{ props }">
-        <v-slide-y-transition>
-          <v-btn
-            v-show="!idle"
-            v-bind="props"
-            color="secondary"
-            size="large"
-            position="fixed"
-            class="mt-2 mr-4"
-            icon
-            style="z-index: 1; right: 0"
-          >
-            <v-icon size="x-large" icon="mdi-table-edit" />
-          </v-btn>
-        </v-slide-y-transition>
-      </template>
-      <v-card>
-        <v-toolbar class="px-2">
-          <v-spacer class="hidden-md-and-down" />
-          <v-text-field
-            v-model="search"
-            label="Rechercher un arrêt de Bus, Métro, RER ou Transilien"
-            hide-details
-            style="min-width: 300px"
-          />
-          <v-spacer class="hidden-md-and-down" />
-          <v-btn large variant="plain" @click="dialog = false">
-            <v-icon size="x-large" icon="mdi-close" />
-          </v-btn>
-        </v-toolbar>
-        <v-card-text style="max-height: calc(100vh - 116px); overflow-y: auto">
-          <v-row justify="center">
-            <v-col cols="12" md="8">
-              <v-card v-if="!items.length" flat class="py-2">
-                <div class="text-center">
-                  <v-icon icon="mdi-chevron-up" color="grey" />
-                </div>
-                <div class="text-center text-grey">
-                  Recherchez vos arrêts de Bus, Métro, Tram, Rer d'île de
-                  France,<br>
-                  pour les ajouter à votre tableau de suivi des prochains
-                  passages.
-                </div>
-              </v-card>
-              <v-card
-                v-for="item in items"
-                :key="item.stop_id"
-                flat
-                class="py-2"
-                @click="toggleStop(item.stop_id)"
-              >
-                <v-row dense>
-                  <v-col class="text-center" cols="1">
-                    <v-icon
-                      v-if="stops.indexOf(item.stop_id) !== -1"
-                      icon="mdi-check"
-                    />
-                  </v-col>
-                  <v-col cols="3" class="text-right py-2">
-                    <v-chip
-                      v-for="line in item.lines"
-                      :key="line"
-                      variant="elevated"
-                      size="small"
-                      label
-                      :class="`font-weight-bold mr-1 ${resolveLineClass(line)}`"
-                    >
-                      {{ line }}
-                    </v-chip>
-                  </v-col>
-                  <v-col cols="8">
-                    <v-list-item
-                      :title="item.stop_name"
-                      :subtitle="item.city"
-                      class="py-0"
-                    />
-                  </v-col>
-                </v-row>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            color="secondary"
-            width="100%"
-            size="x-large"
-            class="font-weight-bold"
-            :rounded="0"
-            variant="tonal"
-            @click="dialog = false"
-          >
-            Fermer
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-main app>
-      <div
-        class="scale-to-screen"
-        :style="
-          scale && {
-            width: `${100/scale}vw`,
-            transform: `scale(${scale})`,
-          }
-        "
-      >
-        <v-card
-          v-for="(stop, index) in prochains_passages"
-          :key="stop.id"
-          flat
+  <v-dialog v-model="dialog" fullscreen transition="dialog-top-transition">
+    <template #activator="{ props }">
+      <v-slide-y-transition>
+        <v-btn
+          v-show="!idle"
+          v-bind="props"
+          color="secondary"
+          size="large"
+          position="fixed"
+          class="mt-2 mr-4"
+          icon
+          style="z-index: 1; right: 0"
         >
-          <v-toolbar
-            v-if="!index || stop.name !== prochains_passages[index-1].name"
-            :class="{
-              'px-3': true,
-              'mt-1': index,
-            }"
-            density="compact"
-            color="grey-lighten-2"
-            :rounded="0"
-            height="54"
-          >
-            <v-toolbar-title>
-              <span class="mr-4">{{ stop.name }}</span>
-              <v-chip
-                v-for="line in stop.lines"
-                :key="line"
-                variant="elevated"
-                size="small"
-                label
-                :class="`font-weight-bold mr-1 ${resolveLineClass(line)}`"
-              >
-                {{ line }}
-              </v-chip>
-            </v-toolbar-title>
-          </v-toolbar>
-          <div v-else class="bg-grey" style="height: 2px" />
-          <v-container fluid class="pa-0">
-            <v-row v-if="!stop.next_stops.length">
-              <v-col class="text-disabled pl-10 py-6 ">
-                Ne circule pas
-              </v-col>
-            </v-row>
-            <v-expand-transition
-              v-for="(prochain_passage, index) in stop.next_stops"
-              :key="prochain_passage"
+          <v-icon size="x-large" icon="mdi-table-edit" />
+        </v-btn>
+      </v-slide-y-transition>
+    </template>
+    <v-card>
+      <v-toolbar class="px-2">
+        <v-spacer class="hidden-md-and-down" />
+        <v-text-field
+          v-model="search"
+          label="Rechercher un arrêt de Bus, Métro, RER ou Transilien"
+          hide-details
+          style="min-width: 300px"
+        />
+        <v-spacer class="hidden-md-and-down" />
+        <v-btn large variant="plain" @click="dialog = false">
+          <v-icon size="x-large" icon="mdi-close" />
+        </v-btn>
+      </v-toolbar>
+      <v-card-text style="max-height: calc(100vh - 116px); overflow-y: auto">
+        <v-row justify="center">
+          <v-col cols="12" md="8">
+            <v-card v-if="!items.length" flat class="py-2">
+              <div class="text-center">
+                <v-icon icon="mdi-chevron-up" color="grey" />
+              </div>
+              <div class="text-center text-grey">
+                Recherchez vos arrêts de Bus, Métro, Tram, Rer d'île de
+                France,<br>
+                pour les ajouter à votre tableau de suivi des prochains
+                passages.
+              </div>
+            </v-card>
+            <v-card
+              v-for="item in items"
+              :key="item.stop_id"
+              flat
+              class="py-2"
+              @click="toggleStop(item.stop_id)"
             >
-              <v-row
-                v-show="
-                  isFuture(
-                    prochain_passage.expected_departure_time ||
-                      prochain_passage.aimed_departure_time
-                  )
-                "
-                no-gutters
-                :class="{
-                  'text-disabled':
-                    prochain_passage.departure_status === 'cancelled',
-                  'bg-grey-lighten-4': index % 2,
-                }"
-              >
-                <v-col
-                  cols="2"
-                  class="font-weight-normal d-none d-sm-flex flex-column align-center justify-center"
-                >
-                  <div
-                    v-if="
-                      !prochain_passage.departure_status &&
-                        prochain_passage.aimed_departure_time
-                    "
-                    class="text-decoration-line-through text-disabled mt-n1 mb-n2"
-                  >
-                    {{ formatTime(prochain_passage.aimed_departure_time) }}
-                  </div>
-                  <div
-                    :class="{
-                      'text-h6': true,
-                      'text-warning mb-n1': !prochain_passage.departure_status,
-                      'text-error text-decoration-line-through':
-                        prochain_passage.departure_status === 'cancelled',
-                    }"
-                  >
-                    {{
-                      formatTime(
-                        prochain_passage.expected_departure_time ||
-                          prochain_passage.aimed_departure_time
-                      )
-                    }}
-                  </div>
+              <v-row dense>
+                <v-col class="text-center" cols="1">
+                  <v-icon
+                    v-if="stops.indexOf(item.stop_id) !== -1"
+                    icon="mdi-check"
+                  />
                 </v-col>
-                <v-col cols="9" sm="8">
-                  <v-list-item density="compact">
-                    <template #prepend>
-                      <v-chip
-                        variant="elevated"
-                        size="small"
-                        label
-                        :class="`mr-4 ${resolveLineClass(
-                          prochain_passage.line
-                        )}`"
-                      >
-                        {{ prochain_passage.line }}
-                      </v-chip>
-                    </template>
-                    <v-list-item-title
-                      :class="{
-                        'text-h6': true,
-                        'text-error text-decoration-line-through':
-                          prochain_passage.departure_status === 'cancelled',
-                      }"
-                    >
-                      {{
-                        prochain_passage.destination_display ||
-                          prochain_passage.destination_name
-                      }}
-                    </v-list-item-title>
-                    <v-list-item-subtitle v-if="prochain_passage.journey_note">
-                      {{ prochain_passage.journey_note }}
-                    </v-list-item-subtitle>
-                  </v-list-item>
+                <v-col cols="3" class="text-right py-2">
+                  <v-chip
+                    v-for="line in item.lines"
+                    :key="line"
+                    variant="elevated"
+                    size="small"
+                    label
+                    :class="`font-weight-bold mr-1 ${resolveLineClass(line)}`"
+                  >
+                    {{ line }}
+                  </v-chip>
                 </v-col>
-                <v-col
-                  cols="3"
-                  sm="2"
-                  :class="{
-                    'text-h6 d-flex align-center justify-center': true,
-                    'text-warning': !prochain_passage.departure_status,
-                    'text-error':
-                      prochain_passage.departure_status === 'cancelled',
-                  }"
-                >
-                  <span v-if="prochain_passage.departure_status === 'cancelled'">Annulé</span>
-                  <span v-else-if="prochain_passage.vehicule_at_stop">A l'arrêt</span>
-                  <span
-                    v-else
-                    v-html="
-                      formatRemainingTime(
-                        prochain_passage.expected_departure_time
-                      )
-                    "
+                <v-col cols="8">
+                  <v-list-item
+                    :title="item.stop_name"
+                    :subtitle="item.city"
+                    class="py-0"
                   />
                 </v-col>
               </v-row>
-            </v-expand-transition>
-          </v-container>
-        </v-card>
-      </div>
-    </v-main>
-    <v-slide-y-reverse-transition>
-      <v-footer
-        color="grey"
-        app
-        class="text-caption"
-        height="24px"
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          color="secondary"
+          width="100%"
+          size="x-large"
+          class="font-weight-bold"
+          :rounded="0"
+          variant="tonal"
+          @click="dialog = false"
+        >
+          Fermer
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <div
+    class="scale-to-screen"
+    :style="
+      scale && {
+        width: `${100/scale}vw`,
+        transform: `scale(${scale})`,
+      }
+    "
+  >
+    <v-card
+      v-for="(stop, index) in prochains_passages"
+      :key="stop.id"
+      flat
+    >
+      <v-toolbar
+        v-if="!index || stop.name !== prochains_passages[index-1].name"
+        :class="{
+          'px-3': true,
+          'mt-1': index,
+        }"
+        density="compact"
+        color="grey-lighten-2"
+        :rounded="0"
+        height="54"
       >
-        <span class="mx-2">© {{ new Date().getFullYear() }}
-          <span class="hidden-sm-and-down">Félicien François</span></span>
-        •
-        <span class="mx-2"><a
-          href="https://github.com/felicienfrancois/idfm-prochains-passages"
-          class="text-white"
-        ><span class="hidden-sm-and-down">Code </span>source</a></span>
-        •
-        <span class="mx-2"><span class="hidden-sm-and-down">Données fournies par l'</span><a
+        <v-toolbar-title>
+          <span class="mr-4">{{ stop.name }}</span>
+          <v-chip
+            v-for="line in stop.lines"
+            :key="line"
+            variant="elevated"
+            size="small"
+            label
+            :class="`font-weight-bold mr-1 ${resolveLineClass(line)}`"
+          >
+            {{ line }}
+          </v-chip>
+        </v-toolbar-title>
+      </v-toolbar>
+      <div v-else class="bg-grey" style="height: 2px" />
+      <v-container fluid class="pa-0">
+        <v-row v-if="!stop.next_stops.length">
+          <v-col class="text-disabled pl-10 py-6 ">
+            Ne circule pas
+          </v-col>
+        </v-row>
+        <v-expand-transition
+          v-for="(prochain_passage, index) in stop.next_stops"
+          :key="prochain_passage"
+        >
+          <v-row
+            v-show="
+              isFuture(
+                prochain_passage.expected_departure_time ||
+                  prochain_passage.aimed_departure_time
+              )
+            "
+            no-gutters
+            :class="{
+              'text-disabled':
+                prochain_passage.departure_status === 'cancelled',
+              'bg-grey-lighten-4': index % 2,
+            }"
+          >
+            <v-col
+              cols="2"
+              class="font-weight-normal d-none d-sm-flex flex-column align-center justify-center"
+            >
+              <div
+                v-if="
+                  !prochain_passage.departure_status &&
+                    prochain_passage.aimed_departure_time
+                "
+                class="text-decoration-line-through text-disabled mt-n1 mb-n2"
+              >
+                {{ formatTime(prochain_passage.aimed_departure_time) }}
+              </div>
+              <div
+                :class="{
+                  'text-h6': true,
+                  'text-warning mb-n1': !prochain_passage.departure_status,
+                  'text-error text-decoration-line-through':
+                    prochain_passage.departure_status === 'cancelled',
+                }"
+              >
+                {{
+                  formatTime(
+                    prochain_passage.expected_departure_time ||
+                      prochain_passage.aimed_departure_time
+                  )
+                }}
+              </div>
+            </v-col>
+            <v-col cols="9" sm="8">
+              <v-list-item density="compact">
+                <template #prepend>
+                  <v-chip
+                    variant="elevated"
+                    size="small"
+                    label
+                    :class="`mr-4 ${resolveLineClass(
+                      prochain_passage.line
+                    )}`"
+                  >
+                    {{ prochain_passage.line }}
+                  </v-chip>
+                </template>
+                <v-list-item-title
+                  :class="{
+                    'text-h6': true,
+                    'text-error text-decoration-line-through':
+                      prochain_passage.departure_status === 'cancelled',
+                  }"
+                >
+                  {{
+                    prochain_passage.destination_display ||
+                      prochain_passage.destination_name
+                  }}
+                </v-list-item-title>
+                <v-list-item-subtitle v-if="prochain_passage.journey_note">
+                  {{ prochain_passage.journey_note }}
+                </v-list-item-subtitle>
+              </v-list-item>
+            </v-col>
+            <v-col
+              cols="3"
+              sm="2"
+              :class="{
+                'text-h6 d-flex align-center justify-center': true,
+                'text-warning': !prochain_passage.departure_status,
+                'text-error':
+                  prochain_passage.departure_status === 'cancelled',
+              }"
+            >
+              <span v-if="prochain_passage.departure_status === 'cancelled'">Annulé</span>
+              <span v-else-if="prochain_passage.vehicule_at_stop">A l'arrêt</span>
+              <span
+                v-else
+                v-html="
+                  formatRemainingTime(
+                    prochain_passage.expected_departure_time
+                  )
+                "
+              />
+            </v-col>
+          </v-row>
+        </v-expand-transition>
+      </v-container>
+    </v-card>
+  </div>
+  <v-slide-y-reverse-transition>
+    <div
+      class="text-caption text-white d-flex bg-grey pa-1"
+      style="height: 26px; position: fixed; bottom: 0; width: 100%;"
+    >
+      <span class="mx-2">© {{ new Date().getFullYear() }}
+        <span class="hidden-sm-and-down">Félicien François</span></span>
+      •
+      <span class="mx-2"><a
+        href="https://github.com/felicienfrancois/idfm-prochains-passages"
+        class="text-white"
+      ><span class="hidden-sm-and-down">Code </span>source</a></span>
+      •
+      <span class="mx-2">
+        <span class="hidden-sm-and-down">Données fournies par l'</span><a
           href="https://prim.iledefrance-mobilites.fr"
           class="text-white"
-        >API Ile de France Mobilité</a></span>
-        <v-spacer />
-        <span class="mx-2 hidden-xs">{{ $vuetify.display.width }}x{{ $vuetify.display.height }}</span>
-        •
-        <span class="mx-2 hidden-xs">{{ formatTimeSec(loadTimestamp) }}</span>
-        •
-        <span class="mx-2 hidden-xs">{{ formatTimeSec(lastRefreshTimestamp) }}</span>
-        •
-        <span class="mx-2 hidden-xs">{{ formatTimeSec(currentTimestamp) }}</span>
-      </v-footer>
-    </v-slide-y-reverse-transition>
-  </v-app>
+        >
+          API Ile de France Mobilité
+        </a></span>
+      <v-spacer />
+      <span class="mx-2 hidden-xs">{{ $vuetify.display.width }}x{{ $vuetify.display.height }}</span>
+      •
+      <span class="mx-2 hidden-xs">{{ formatTimeSec(loadTimestamp) }}</span>
+      •
+      <span class="mx-2 hidden-xs">{{ formatTimeSec(lastRefreshTimestamp) }}</span>
+      •
+      <span class="mx-2 hidden-xs">{{ formatTimeSec(currentTimestamp) }}</span>
+    </div>
+  </v-slide-y-reverse-transition>
 </template>
 <script lang="ts">
 import debounce from "@/utils/debounce";
