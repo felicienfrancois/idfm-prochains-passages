ba172e99b6921b04f1cef6f5aabfad5d5afc1c71
Félicien François <felicien@alvarum.com>
2023-03-03 23:04:20 +0100

    improve footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,32 +1,33 @@
 <template>
   <div class="flex fixed z-40 bottom-0 h-6 px-1 py-1 w-full bg-stone-300 text-[12px] text-stone-500">
-    <span class="mx-2">
-      © {{ currentTime.getFullYear() }}
-      <span class="hidden md:inline">Félicien François</span>
-    </span>
-    •
-    <span class="mx-2">
-      <a href="https://github.com/felicienfrancois/idfm-prochains-passages" class="text-stone-600 underline">
-        <span class="hidden md:inline">Code</span>
-        source
-      </a>
-    </span>
-    •
-    <span class="mx-2">
-      <span class="hidden lg:inline">Données fournies par l'</span>
-      <a
-        href="https://prim.iledefrance-mobilites.fr"
-        class="text-stone-600 underline"
-      >API Ile de France Mobilité</a>
-    </span>
-    <span class="grow" />
-    <span class="mx-2 hidden sm:inline">{{ displayWidth }}x{{ displayHeight }}</span>
-    •
-    <span class="mx-2 hidden sm:inline">{{ formatTimeSec(loadTime) }}</span>
-    •
-    <span class="mx-2 hidden sm:inline">{{ formatTimeSec(lastRefreshTime) }}</span>
-    •
-    <span class="mx-2 hidden sm:inline">{{ formatTimeSec(currentTime) }}</span>
+    <div>
+      <span class="mx-2">
+        © {{ currentTime.getFullYear() }}
+        <span class="hidden sm:inline">Félicien François</span>
+      </span>
+      <span>•</span>
+      <a href="https://prochains-passages.fr" class="mx-2 text-stone-600 underline">prochains-passages.fr</a>
+      <span class="hidden lg:inline">•</span>
+      <a href="https://github.com/felicienfrancois/idfm-prochains-passages" class="hidden lg:inline mx-2 text-stone-600 underline">Code source</a>
+      <span>•</span>
+      <span class="mx-2">
+        <span class="hidden lg:inline">Données fournies par l'</span>
+        <a
+          href="https://prim.iledefrance-mobilites.fr"
+          class="text-stone-600 underline"
+        >API <span class="hidden sm:inline">Ile de France</span><span class="sm:hidden">IDF</span> Mobilité</a>
+      </span>
+    </div>
+    <div class="grow" />
+    <div class="hidden md:inline">
+      <span class="mx-2">{{ displayWidth }}x{{ displayHeight }}</span>
+      <span>•</span>
+      <span class="mx-2">{{ formatTimeSec(loadTime) }}</span>
+      <span>•</span>
+      <span class="mx-2">{{ formatTimeSec(lastRefreshTime) }}</span>
+      <span>•</span>
+      <span class="mx-2">{{ formatTimeSec(currentTime) }}</span>
+    </div>
   </div>
 </template>
 
